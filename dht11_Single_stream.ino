52c0afcc0a9c5eb064b88d8db1b2d59b3790887d
Yildirimm <hakanyildirim_28@msn.com>
2021-01-27 22:58:56 +0300

    Add files via upload
    shows the single data transfer with dht11

---
+++ dht11_Single_stream.ino
@@ -0,0 +1,206 @@
+
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor 
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+#define WIFI_SSID "a.Cucumber"
+#define WIFI_PASSWORD "Cucumber123"
+
+#define FIREBASE_HOST "dht11-4b097-default-rtdb.firebaseio.com"
+
+/** The database secret is obsoleted, please use other authentication methods,
+   see examples in the Authentications folder.
+*/
+#define FIREBASE_AUTH "NYqaVUXCx35ARjuswlzhlX3Ybhsr5nNaNltCEH7w"
+
+//Define FirebaseESP8266 data object
+FirebaseData fbdo;
+
+
+unsigned long sendDataPrevMillis = 0;
+
+String path = "/Test/Stream";
+
+// uint16_t count = 0;
+
+void printResult(FirebaseData &data);  /// ileride tanımlayacak
+
+
+
+
+
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  // Initialize the DHT11.
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print TEMPERATURE sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  /*Serial.println(F("------------------------------------"));
+    Serial.println(F("Temperature Sensor"));
+    Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+    Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+    Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+    Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+    Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+    Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+    Serial.println(F("------------------------------------"));*/
+  // Print HUMIDITY sensor details.
+  dht.humidity().getSensor(&sensor);
+  /* Serial.println(F("Humidity Sensor"));
+    Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+    Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+    Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+    Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+    Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+    Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+    Serial.println(F("------------------------------------"));*/
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+  Serial.println("the delay"+ delayMS);
+
+
+  /// set the WIFI connection
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  //Set the size of WiFi rx/tx buffers in the case where we want to work with large data.
+  fbdo.setBSSLBufferSize(1024, 1024);
+
+  //Set the size of HTTP response buffers in the case where we want to work with large data.
+  fbdo.setResponseSize(1024);
+
+  if (!Firebase.beginStream(fbdo, path))
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Can't begin stream connection...");
+    Serial.println("REASON: " + fbdo.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
+
+void loop()
+{
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+  /// send the data
+  if (millis() - sendDataPrevMillis > 15000)
+  {
+    sendDataPrevMillis = millis();
+
+    Serial.println("------------------------------------");
+    Serial.println("Sending as string...");
+    if (Firebase.setString(fbdo, path + "/dhtHumidity", String(event.relative_humidity)))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+      Serial.print("VALUE: ");
+      printResult(fbdo);
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+
+  }
+
+  if (!Firebase.readStream(fbdo))
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Can't read stream data...");
+    Serial.println("REASON: " + fbdo.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+
+  if (fbdo.streamTimeout())
+  {
+    Serial.println("Stream timeout, resume streaming...");
+    Serial.println();
+  }
+
+  if (fbdo.streamAvailable())
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Stream Data available...");
+    Serial.println("STREAM PATH: " + fbdo.streamPath());
+    Serial.println("EVENT PATH: " + fbdo.dataPath());
+    Serial.println("DATA TYPE: " + fbdo.dataType());
+    Serial.println("EVENT TYPE: " + fbdo.eventType());
+    Serial.print("VALUE: ");
+    printResult(fbdo);
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
+
+void printResult(FirebaseData &data)
+{
+  Serial.println();
+  //Serial.println("printing result...");
+  if (data.dataType() == "int")
+    Serial.println(data.intData());
+
+  else
+  {
+    Serial.println(data.payload());
+  }
+}
